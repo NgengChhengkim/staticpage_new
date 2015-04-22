bdc24e8413158915cd18a2b93924f4e539f034ac
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-04-22 11:16:40 +0700

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,8 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-     password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   test "should be valid" do
     assert @user.valid?
@@ -56,5 +57,7 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-end
+   test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
